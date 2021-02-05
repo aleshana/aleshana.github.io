ad6af5cc731e88210df6f70c0e6cff33eddba962
aleshana <aleshana@byui.edu>
2021-02-04 18:19:09 -0700

    first save week 5

---
+++ lesson05/js/script.js
@@ -0,0 +1,14 @@
+// var todaysDate = new Date();
+// var todaysYear = todaysDate.getFullYear();
+// console.log(todaysYear);
+// document.getElementById("currentYear").innerHTML = todaysYear;
+
+// var dateTimeValue = document.lastModified;
+// console.log("dateTimeValue = " + dateTimeValue);
+// document.getElementById('indexUpdate').innerHTML = "Last Updated  " + " " + dateTimeValue;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
---
+++ lesson05/js/scripts.js
@@ -0,0 +1,32 @@
+
+
+// Todays Current Date
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var d = new Date();
+var todaysWeekDay = days[d.getDay()];
+var todaysDay = d.getDate();
+var todaysMonth = months[d.getMonth()];
+var todaysYear = d.getFullYear();
+
+var fullDate = todaysWeekDay + ", " + todaysMonth + " " + todaysDay + ", " + todaysYear;
+
+console.log(fullDate);
+document.getElementById("currentDate").innerHTML = fullDate;
+
+
+// Toggle Function
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+//Current Year
+var todaysDate = new Date();
+var todaysYear = todaysDate.getFullYear();
+console.log(todaysYear);
+document.getElementById("currentYear").innerHTML = todaysYear;
+
+
