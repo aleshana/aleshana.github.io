af017720a2f4fe5dc23896186ad4d21996b0f1b7
aleshana <aleshana@byui.edu>
2021-03-01 22:37:58 -0700

    Fix Activity API Obeserver

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -1,35 +1,32 @@
-// Lazy loading images
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-const imgOptions = {
-    rootMargin: '0px 0px 50px 0 px' ,
-    threshold: 0
-};
-
-if ('intersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items) => {
-        items.forEach((item) => {
-            if (item.isInterecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOption);
 imagesToLoad.forEach((img) => {
-    imgObserver.observe(img);  
-});
+    loadImages(img);
+  });
 
-} else {
-    imagesToLoad.forEach ((img) => {
-        loadImages(img);
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
-}
-
-
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } 
+  else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
