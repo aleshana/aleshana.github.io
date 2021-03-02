d2e7d62e6f15e31200dc29cd373ff38e919c5fa8
aleshana <aleshana@byui.edu>
2021-03-02 07:16:31 -0700

    Final working API fix

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -1,33 +1,49 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+// Lazy loading images
+
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 1
 };
 
 imagesToLoad.forEach((img) => {
     loadImages(img);
   });
+  
 
-  
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
+if ('IntersectionObserver' in window) {
+    console.log("testing123");
+
+    const imgObserver = new IntersectionObserver(items => {
+        console.log("Number of items " + items.length);
+        items.forEach((item) => {
+            console.log("In forEach ");
+            if (item.isInterecting) {
+                console.log(item.isIntersecting);
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            } 
+            else {
+                console.log("Can't see image")
+            }
+        });
+    }, imgOptions);
+
     imagesToLoad.forEach((img) => {
-      observer.observe(img);
+        console.log("In imagesToLoad.forEach");
+        imgObserver.observe(img);  
     });
-  } 
-  else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
+} else {
+    imagesToLoad.forEach ((img) => {
+        loadImages(img);
     });
-  }
-  
+}
+
+
