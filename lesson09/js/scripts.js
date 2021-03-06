758217c87f3cc3ca04db0ce7b1e4066062f5af97
aleshana <aleshana@byui.edu>
2021-03-06 14:21:05 -0700

    homepage-small completed

--- lesson09/js/scripts.js
+++ lesson09/js/scripts.js
@@ -55,4 +55,60 @@ WebFont.load({
   function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-  
+
+
+/*********JSON TOWN DATA LOADING***********/
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const towns = jsonObject['towns']; //Store data in array
+
+    const boxes = document.querySelector('div.townsDiv');  /// the boxes location in HTML
+
+    const townsFeatured = new Array();
+    console.table(townsFeatured);
+    for (let i=0; i < towns.length; i++){
+        if 
+        (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven') {
+          townsFeatured.push(towns[i]);
+            }
+
+    }
+    console.table(townsFeatured);
+    townsFeatured.forEach(town => {
+        let box = document.createElement('section');
+        let h3 = document.createElement('h3');
+        let motto = document.createElement('h6');
+        let para = document.createElement('p');
+        let image = document.createElement('img');
+
+
+        h3.innerHTML = town.name;
+        motto.innerHTML =  town.motto;   
+        para.innerHTML = `Year Founded: ${town.yearFounded} <br>
+        Population: ${town.currentPopulation}<br>
+        Annual Rain Fall: ${town.averageRainfall}`;
+               
+        box.setAttribute ('class',"homeTownBox");
+        h3.setAttribute ('class',"homeTownHeader");
+        motto.setAttribute ('class',"homeTagline");
+        para.setAttribute ('class',"homePara");
+        image.setAttribute ('src', "images/" + town.photo);
+        image.setAttribute ('alt', `Photo from the town of ${town.name}.`);
+        image.setAttribute ('class',"homeTownImg");
+
+
+        box.appendChild(h3);  //appendChild is h2 to card
+        box.appendChild(motto);
+        box.appendChild(para); 
+        box.appendChild(image);
+        boxes.append(box);
+
+    });
+});
