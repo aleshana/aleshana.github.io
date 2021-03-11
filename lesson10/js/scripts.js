c195a82dd78ca60c13996118f452e3ea75eec80c
aleshana <aleshana@byui.edu>
2021-03-11 11:33:49 -0700

    Fixed live weather data, integrated windchill js.

--- lesson10/js/scripts.js
+++ lesson10/js/scripts.js
@@ -104,11 +104,70 @@ fetch(requestURL)
         image.setAttribute ('class',"homeTownImg");
 
 
-        box.appendChild(h3);  //appendChild is h2 to card
+        box.appendChild(h3);  //append h3 to each town section
         box.appendChild(motto);
         box.appendChild(para); 
         box.appendChild(image);
         boxes.append(box);
-
+        
     });
 });
+
+/*********JSON CURRENT WEATHER DATA***********/
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e73db471f9317b7f3bf9d5f6c28904fc&units=imperial';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then ((jsObject) => {
+    console.table(jsObject);  // temporary checking for valid response and data parsing
+    
+    document.getElementById('windSpeed').textContent = jsObject.wind.speed;
+    document.getElementById('currentTemp').textContent = jsObject.main.temp;
+    document.getElementById('currentDesc').textContent = jsObject.weather[0].main;
+    document.getElementById('humidity').textContent =  jsObject.main.humidity;
+
+    /*************** Wind Chill Calculations and outputing *************/
+    var Temperature = jsObject.main.temp;
+    var WindSpeed = jsObject.wind.speed;
+    // console.log(Temperature);
+  
+    var answer = windChill(Temperature, WindSpeed);
+    document.getElementById('windChillOutput').innerHTML = "Wind Chill:  " + answer;
+
+    function windChill(tempF, speed) {
+      var wc = "N/A";
+      if (tempF < 50 & speed > 4.8) {
+          wc = 35.74 + (0.6215 * tempF) - (35.75 * ( Math.pow(speed, .16))) + (.4275 * tempF) * (Math.pow(speed, .16));
+          // console.log(wc);
+          // console.log(tempF);
+          // console.log(speed);
+          wc = wc.toFixed(2)
+        }   
+      return wc;
+      
+    }
+
+});
+
+/*********JSON FORECAST DATA***********/
+// let cityID = '5604473';
+// let keyID = 'e73db471f9317b7f3bf9d5f6c28904fc';
+let apiWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e73db471f9317b7f3bf9d5f6c28904fc';
+
+fetch(apiWeatherURL)
+  .then((response) => response.json())
+  .then ((jsObject2) => {
+    console.table(jsObject2); 
+
+    let forecastDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+//   const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject2.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
+
+  });
+
+  
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,17 +1,17 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e73db471f9317b7f3bf9d5f6c28904fc';
-const jsObject = new Array;
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e73db471f9317b7f3bf9d5f6c28904fc&units=imperial';
+// const jsObject = new Array;
 
 fetch(apiURL)
   .then((response) => response.json())
   .then ((jsObject) => {
     console.table(jsObject);  // temporary checking for valid response and data parsing
-    console.table(jsObject.list);
+    console.table(jsObject.main.temp);
+    // console.table(jsObject.city);  //testing json data.
 
-  console.table(jsObject.city);  //testing json data.
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-  document.getElementById('current-temp').innerHTML = jsObject.main.temp;
 
-  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
