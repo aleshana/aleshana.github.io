59e8a02e7676e28dd120a92fca482b847817384b
aleshana <aleshana@byui.edu>
2021-03-18 15:39:38 -0600

    update Lesson 11 lazyload

--- lesson11/js/lazyload.js
+++ lesson11/js/lazyload.js
@@ -13,31 +13,31 @@ const imgOptions = {
     threshold: 1
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
   
 
 if ('IntersectionObserver' in window) {
-    console.log("testing123");
+    // console.log("testing123");
 
     const imgObserver = new IntersectionObserver(items => {
-        console.log("Number of items " + items.length);
+        // console.log("Number of items " + items.length);
         items.forEach((item) => {
-            console.log("In forEach ");
-            if (item.isInterecting) {
-                console.log(item.isIntersecting);
+            // console.log("In forEach ");
+            if (item.isIntersecting) {
+                // console.log(item.isIntersecting);
                 loadImages(item.target);
                 imgObserver.unobserve(item.target);
             } 
-            else {
-                console.log("Can't see image")
-            }
+            // else {
+            //     console.log("Can't see image")
+            // }
         });
     }, imgOptions);
 
     imagesToLoad.forEach((img) => {
-        console.log("In imagesToLoad.forEach");
+        // console.log("In imagesToLoad.forEach");
         imgObserver.observe(img);  
     });
 } else {
