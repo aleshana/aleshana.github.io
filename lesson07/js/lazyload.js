59fcdfa173c58172a6563d24cbb93c6a87f75de2
aleshana <aleshana@byui.edu>
2021-03-18 15:35:02 -0600

    lazyload js code fix

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -4,28 +4,29 @@ const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');
+    image.onload = () => { 
+        image.removeAttribute('data-src');
     };
 };
 
 const imgOptions = {
     rootMargin: '0px 0px 50px 0px',
-    threshold: 1
+    threshold: 0
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
   
 
 if ('IntersectionObserver' in window) {
     console.log("testing123");
 
-    const imgObserver = new IntersectionObserver(items => {
+    const imgObserver = new IntersectionObserver((items) => {
         console.log("Number of items " + items.length);
         items.forEach((item) => {
             console.log("In forEach ");
-            if (item.isInterecting) {
+            if (item.isIntersecting) {
                 console.log(item.isIntersecting);
                 loadImages(item.target);
                 imgObserver.unobserve(item.target);
