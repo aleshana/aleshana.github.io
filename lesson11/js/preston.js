305533da3a8d62829d34ebbdfc36547f8c96793f
aleshana <aleshana@byui.edu>
2021-03-20 22:09:24 -0600

    fix js code for Preston -2 try

--- lesson11/js/preston.js
+++ lesson11/js/preston.js
@@ -13,19 +13,18 @@ const prestonCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?id=56
 fetch(prestonCurrentURL)
 .then((response) => response.json())
 .then ((jsObject) => {
-  console.table(jsObject);  // temporary checking for valid response and data parsing
   
   document.getElementById('windSpeedPreston').textContent = jsObject.wind.speed.toFixed(0);
   document.getElementById('currentTempPreston').textContent = jsObject.main.temp.toFixed(0);
   document.getElementById('currentDescPreston').textContent = jsObject.weather[0].main;
   document.getElementById('humidityPreston').textContent =  jsObject.main.humidity;
 
-  console.log(jsObject.wind.speed.toFixed(0));
+  
 
   /*************** Wind Chill Calculations and outputing *************/
   var Temperature = jsObject.main.temp;
   var WindSpeed = jsObject.wind.speed;
-  console.log(Temperature);
+
 
   var answer = windChill(Temperature, WindSpeed);
   document.getElementById('windChillPreston').innerHTML = "Wind Chill:  " + answer + " &#8457;";
@@ -34,9 +33,7 @@ fetch(prestonCurrentURL)
     var wc = "N/A";
     if (tempF < 50 & speed > 4.8) {
         wc = 35.74 + (0.6215 * tempF) - (35.75 * ( Math.pow(speed, .16))) + (.4275 * tempF) * (Math.pow(speed, .16));
-        console.log(wc);
-        console.log(tempF);
-        console.log(speed);
+      
         wc = wc.toFixed(0)
       }   
     return wc;
@@ -53,20 +50,16 @@ let apiWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473
 fetch(apiWeatherURL)
 .then((response) => response.json())
 .then ((jsObject2) => {
-  console.table(jsObject2); 
-
-  // console.log(jsObject2.list[0].dt_txt);
+  
 
   var forecast5 = []     //create new array of just 18:00:00 objects
   for (i = 0; i< jsObject2.list.length; i++) {
    
-    if (jsObject2.list[i].dt_txt.includes('18:00:00')) {                  ///.clouds.dt_txt === "*18:00:00")
-      // console.log(jsObject2.list[i].dt_txt);
+    if (jsObject2.list[i].dt_txt.includes('18:00:00')) {                  
+    
       forecast5.push(jsObject2.list[i]);
     }
-    // else {
-    //   console.log(jsObject2.list[i].dt_txt);
-    // }
+    
   }
   console.log(forecast5);  //check new array
 
@@ -80,7 +73,7 @@ fetch(apiWeatherURL)
     days5.push(forecastDays[forecastIndex]);
     
   }
-  console.log(days5);
+  
 
   //publish 5-day data to webpage
   var strForecast = "";
@@ -105,7 +98,7 @@ fetch(townDataURL)
   return response.json();
 })
 .then(function (jsonObject) {
-  console.table(jsonObject);  // temporary checking for valid response and data parsing
+  
 
       let towns = jsonObject['towns']; //Store data in array
 
@@ -116,7 +109,7 @@ fetch(townDataURL)
            }  //if bracket
       }    //for bracket
 
-    console.table(townPreston);
+    
 
     const eventOutput  = document.querySelector('.gridEventsPreston1');  /// the output location in HTML
       let strCityEvents = "";
@@ -126,14 +119,13 @@ fetch(townDataURL)
       let eventPara = document.createElement('p');
       strCityEvents = townPreston.name.split(" ").join("");
     
-        console.log(townPreston.events);
+        
 
       for (i = 0; i< townPreston.events.length; i++) {
           strEvents = strEvents += townPreston.events[i] + "<br><br>";
           }
 
-      console.log(strEvents);
-      console.log(strCityEvents);
+      
 
       eventPara.innerHTML = strEvents;
       eventPara.setAttribute ('class',"cityEventPara");
