fe505b5e6c19c0665152fb61b7442fd521679187
aleshana <aleshana@byui.edu>
2021-04-04 20:38:59 -0600

    JS script directory & fix image names

--- MapletonChamber/js/businesses.js
+++ MapletonChamber/js/businesses.js
@@ -9,4 +9,49 @@ fetch(getDirectory)
 
 const companies = jsonDir['companies'];
 
+const cards = document.querySelector('div.directGrid'); 
+
+    companies.forEach(company => {
+        let card = document.createElement('section');
+        let name = document.createElement('h4');
+        let logo = document.createElement('img');
+        let phone = document.createElement('div');
+        let address = document.createElement('div');
+        let emailLink = document.createElement('div');
+        let urlLink = document.createElement('div');
+        
+        
+
+        // let email = company.email;
+        // let url = company.url;
+
+        logo.innerHTML = ""
+        name.innerHTML = company.name;
+        phone.innerHTML = company.phone;
+        address.innerHTML = company.address;
+        emailLink.innerHTML = `<a href = "mailto:${company.email}">Email</a>`;
+        urlLink.innerHTML = `<a href = "${company.url}">Website</a>`;
+
+        logo.setAttribute ('src', `images/${company.logo}`);
+        logo.setAttribute ('alt', `${company.name} Logo`);
+        logo.setAttribute ('class', "directLogo");
+        
+
+        card.appendChild(name);
+        card.appendChild(logo); 
+        
+        card.appendChild(phone);
+         
+        card.appendChild(address);
+        
+        card.appendChild(emailLink);
+        
+        card.appendChild(urlLink);
+        
+        
+        cards.append(card);
+
+
+    });
+
 });
